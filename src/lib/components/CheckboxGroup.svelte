5b43e5797b167e75fcf37e7cee8ebdd451064322
Dan McAtee <danielmcatee@me.com>
2023-04-24 11:19:37 +0200

    add seperator

--- src/lib/components/CheckboxGroup.svelte
+++ src/lib/components/CheckboxGroup.svelte
@@ -1,32 +1,36 @@
 <script lang="ts">
 	export let group: string[];
-	export let inputs: { label: string; addition?: string; value: string }[];
+	export let inputs: ({ label: string; addition?: string; value: string } | null)[];
 	export let name: string;
 
 	let otherValue: string = '';
 	let checkboxRef: HTMLInputElement | null = null;
 </script>
 
-{#each inputs as { label, addition, value }}
-	<label class="flex items-center gap-3">
-		{#if label !== 'Other'}
-			<input type="checkbox" {value} bind:group {name} />
-		{:else}
-			<input type="checkbox" value={otherValue} bind:this={checkboxRef} bind:group {name} />
-		{/if}
-		<span class="text-sm shrink-0">
-			{label}&nbsp;
-			{#if addition}
-				<span class="text-description text-xs">({addition})</span>
+{#each inputs as input}
+	{#if input}
+		<label class="flex items-center gap-3">
+			{#if input.label !== 'Other'}
+				<input type="checkbox" value={input.value} bind:group {name} />
+			{:else}
+				<input type="checkbox" value={otherValue} bind:this={checkboxRef} bind:group {name} />
 			{/if}
-		</span>
-		{#if label === 'Other'}
-			<input
-				type="text"
-				placeholder="Tell us more..."
-				bind:value={otherValue}
-				class="bg-input rounded-md text-sm px-3 py-2 border-none w-full placeholder:text-placeholder"
-			/>
-		{/if}
-	</label>
+			<span class="text-sm shrink-0">
+				{input.label}&nbsp;
+				{#if input.addition}
+					<span class="text-description text-xs">({input.addition})</span>
+				{/if}
+			</span>
+			{#if input.label === 'Other'}
+				<input
+					type="text"
+					placeholder="Tell us more..."
+					bind:value={otherValue}
+					class="bg-input rounded-md text-sm px-3 py-2 border-none w-full placeholder:text-placeholder"
+				/>
+			{/if}
+		</label>
+	{:else}
+		<div class="h-px bg-min-contrast-gray col-span-2 opacity-30 w-1/3" />
+	{/if}
 {/each}
