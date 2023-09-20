9bf7ded1691c2031ceb6dcf4cfbc591dbc7fe56f
Dan McAtee <danielmcatee@me.com>
2023-09-20 09:42:53 +0200

    test map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
-<div class="h-screen w-screen flex items-center justify-center">
-	<a href="/general-information">Start</a>
+<script lang="ts">
+	import type { LatLngExpression } from 'leaflet';
+	import Leaflet from '$lib/components/map/Leaflet.svelte';
+	import Marker from '$lib/components/map/Marker.svelte';
+	import Popup from '$lib/components/map/Popup.svelte';
+
+	const initialView: LatLngExpression = [51.514244, 7.468429]; // Dortmund, Germany
+	const markerLocations: Array<LatLngExpression> = [
+		[51.513870009926, 7.473969975241] // ShipBit Office
+	];
+</script>
+
+<div class="w-full h-screen">
+	<Leaflet view={initialView} zoom={14}>
+		{#each markerLocations as latLng}
+			<Marker {latLng} width={40} height={40}>
+				<img src="/map-marker.svg" alt="Map marker" class="h-10 w-10" />
+				<Popup>Like & Subscribe!</Popup>
+			</Marker>
+		{/each}
+	</Leaflet>
 </div>
