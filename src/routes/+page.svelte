10de11f6e01747bb19a4c08b1d60428367a80ef8
Dan McAtee <danielmcatee@me.com>
2024-05-27 08:56:38 +0200

    add images locally

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,28 +5,28 @@
 	const datasets = [
 		{
 			title: 'Title',
-			img: 'https://s3-alpha-sig.figma.com/img/4ac2/f682/9874887e3f026d3b468bdcc82f822241?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AA6rCVkKf8A3XJH2z5XIsJ38WdqkqCm7EHGqjdXZP2FNEyInYt5p857ipXhNbtSvRRYwAMNk336WpuwRqMVD8y2Mdl86zLNicdhc6n0S2fW1slTuMWq0eEIx8LV6TdM0VtjpSokTYU-bKwk6C7GTtd-7vmBMnZ0LIf-5vycjhg8WE7kzsOFdy-LZhdyEWzBWiUYZ4KgupCzWolwsdeZsDWZoXjljI0cg6CpfFsI2brPIEIAGifr4vTT9Pu-2taIXuccAgiDJ3B7bdPaZCwR20aNA5GF1Ll8YIJpGTwz7k~rRChalR4dxgSYDcXPks~wk0J2SLJ6SxZT~CQOJ8uNLxg__',
+			img: 'flower.jpeg',
 			date: 'Date',
 			excerpt:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec viverra  elit. Sed vitae libero vel orci venenatis rhoncus ut vel augue. Nam vel  suscipit nibh.'
 		},
 		{
 			title: 'Title',
-			img: 'https://s3-alpha-sig.figma.com/img/551a/2b6f/1a57561974b45dc755998daabc7ca299?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=e3VQBqPkKO628PsiRKdOMzjHq4koNkGRTZJtS2W9CA7tkSrO1fDQt~VjBS-6-YVgEyYQ-vyMWhYv6KtheO5bto2os8ZBi54xkJkLfOWbg~rld~LIqobnvA-BQ04oJyf--8b7U7KCyE4K5G6bana3xn9aYM2YZwiJ-TIgM7gS1owVuHS5gzspfhor~8vOSdZ7UzaO8NqaFrCslaFU3WHy4I6QUJsS2hGqA6dwboULvreKuGdKNzRtcPtQIy3w-Xi61DJrikQ0EHLDervMR7E07iasgccKDLf1yEzs90z2DQxRZOoT8EpBtgs5-JNZcPvoHBr6Uf~C9NfcEtTqHfuxCA__',
+			img: 'spider.jpeg',
 			date: 'Date',
 			excerpt:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec viverra  elit. Sed vitae libero vel orci venenatis rhoncus ut vel augue. Nam vel  suscipit nibh.'
 		},
 		{
 			title: 'Title',
-			img: 'https://s3-alpha-sig.figma.com/img/0187/f4ca/c33447af451ff29971d1282ac671a2a1?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n7R4vA9kYDv6THcwm36~9e0n82LJ6KA6PiU921Bz-el2hzhCzzruXziPWNECI3Eg3IDtLvTpKaZndU98JhtYGtvPfLoLKGZv9-L~Y6-0xMGsuc-StugaUm8r5SkuYOKw4nwVNz2DBX85g8ugYbB9x5um8CvniJ4t87qbLEdvyKw~G3ujuPrQaQzQBVknNNsPzbwtyTkRvQ7ZL6Y0bsvO6GTrqcn-KjbY5eovHplnG1j-1HtRoka4-5YVVHp8b0NeDgq8QnIwgq4VRpQkt3hH2YWoRM~jsQKcWDfE5uquDJMUNJDa25Cu7OnSEtkKTLKdYTzQKqNA6ToGeYBlUW-6QQ__',
+			img: 'fungus.png',
 			date: 'Date',
 			excerpt:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec viverra  elit. Sed vitae libero vel orci venenatis rhoncus ut vel augue. Nam vel  suscipit nibh.'
 		},
 		{
 			title: 'Title',
-			img: 'https://s3-alpha-sig.figma.com/img/cf30/c406/5a2d75da2f048cdd87026ed8cd5aafb5?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=afpSCt7Mp~07p8NSoVANzzW1QU7XGNMVaZUAM4ikm2n~qsKk01Zx0crM1o7wK0sbsJvrsRLZaFe3vKCIrJcB0Ez42XeBstOwUBBSfJJDQdh6SHcHjrOAdXUUHs--3EXQxbIIRlvdDDTYgLXlu2-X5w3Bo4m8y9v6vvVB4CLUXn7rY3Odu2t2B-hfkJlivy7NW~x762AYNdYqmFlEe54aEg2o8PAtq~c6p7FUsAwEeg4ow52dPl5qvVM97q5aYNartM62fvw8NZFxqn1cEtMWkuch7UHaGFq1tbDr2a2jIUNqj~xW9UKZPHNLRg3BOT1k98TVfdej8pIFWMWrpYeGhA__',
+			img: 'fish.jpeg',
 			date: 'Date',
 			excerpt:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec viverra  elit. Sed vitae libero vel orci venenatis rhoncus ut vel augue. Nam vel  suscipit nibh.'
