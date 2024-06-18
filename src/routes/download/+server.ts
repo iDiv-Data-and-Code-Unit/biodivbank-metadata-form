8b3be0d6e023581abd3b7aeeba2b3068c830974c
Dan McAtee <danielmcatee@me.com>
2024-06-18 13:00:07 +0200

    fix download

--- src/routes/download/+server.ts
+++ src/routes/download/+server.ts
@@ -1,10 +1,12 @@
 import fs from 'fs/promises';
+import path from 'path';
 
 export async function GET({ url }) {
 	const ext = url.searchParams.get('ext');
 	console.log(ext);
 
-	const file = await fs.readFile(`files/template.${ext}`);
+	const file = await fs.readFile(path.join(process.cwd(), `files/template.${ext}`));
+
 	console.log(file);
 	return new Response(file, {
 		status: 200,
