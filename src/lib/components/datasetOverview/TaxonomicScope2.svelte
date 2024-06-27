da02e4e621c0fbb5fb258819dcc6cfa6a4b8e4d1
Ibrahim Mammadov <zxyctn@gmail.com>
2024-06-27 14:04:28 +0200

    Convert Taxonomic Scope to Multistep

---
+++ src/lib/components/datasetOverview/TaxonomicScope2.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Multistep from '../Multistep.svelte';
+	import Kingdoms from './Kingdoms.svelte';
+	import Phylum from './Phylum.svelte';
+</script>
+
+<Multistep question="Which kingdoms are involved?" steps=2>
+	<Kingdoms />
+	<div slot="step-2" class="grid gap-8">
+		<Phylum />
+	</div>
+</Multistep>
