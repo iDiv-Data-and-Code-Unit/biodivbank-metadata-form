c9cbc0bd5d8f1727f08718a9ba364181ccc790b6
Ibrahim Mammadov <zxyctn@gmail.com>
2024-11-29 13:39:19 +0100

    Convert RiverCrossSection from an array to a single object

--- src/lib/components/sampling-design/RiverCrossSection.svelte
+++ src/lib/components/sampling-design/RiverCrossSection.svelte
@@ -9,13 +9,13 @@
 		const checked = target.checked;
 
 		if (checked) {
-			$samplingDesign.samplingLocation.riverCrossSection[0].type = [
-				...$samplingDesign.samplingLocation.riverCrossSection[0].type,
+			$samplingDesign.samplingLocation.riverCrossSection.type = [
+				...$samplingDesign.samplingLocation.riverCrossSection.type,
 				type
 			];
 		} else {
-			$samplingDesign.samplingLocation.riverCrossSection[0].type =
-				$samplingDesign.samplingLocation.riverCrossSection[0].type.filter((t) => t !== type);
+			$samplingDesign.samplingLocation.riverCrossSection.type =
+				$samplingDesign.samplingLocation.riverCrossSection.type.filter((t) => t !== type);
 		}
 	};
 </script>
@@ -30,13 +30,13 @@
 		<label
 			class={clsx(
 				'absolute top-1 left-7 text-center',
-				$samplingDesign.samplingLocation.riverCrossSection[0].bank ? 'w-32' : 'w-10'
+				$samplingDesign.samplingLocation.riverCrossSection.bank ? 'w-32' : 'w-10'
 			)}
 		>
 			Bank
 			<select
 				class="text-sm py-1 px-2 bg-white w-full rounded-md border border-description"
-				bind:value={$samplingDesign.samplingLocation.riverCrossSection[0].bank}
+				bind:value={$samplingDesign.samplingLocation.riverCrossSection.bank}
 			>
 				<option value="" disabled />
 				<option value="onBank">On bank</option>
@@ -46,7 +46,7 @@
 		<label
 			class={clsx(
 				'absolute top-1 text-center',
-				$samplingDesign.samplingLocation.riverCrossSection[0].surface
+				$samplingDesign.samplingLocation.riverCrossSection.surface
 					? 'w-36 left-[15rem]'
 					: 'w-10 left-[19rem]'
 			)}
@@ -54,7 +54,7 @@
 			Surface
 			<select
 				class="text-sm py-1 px-2 bg-white w-full rounded-md border border-description"
-				bind:value={$samplingDesign.samplingLocation.riverCrossSection[0].surface}
+				bind:value={$samplingDesign.samplingLocation.riverCrossSection.surface}
 			>
 				<option value="" disabled />
 				<option value="onSurface">On surface</option>
@@ -64,12 +64,10 @@
 		<label class="flex items-center gap-3 absolute -top-10 right-10">
 			<input
 				type="checkbox"
-				checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes(
-					'Riparian Zone'
-				)}
+				checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Riparian Zone')}
 				on:change={(e) => checkHandler(e, 'Riparian Zone')}
 				value="Riparian Zone"
-				bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+				bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 			/>
 			<span class="text-sm shrink-0">Riparian zone&nbsp; </span>
 		</label>
@@ -77,23 +75,23 @@
 			<span class="text-sm shrink-0"> Water&nbsp; </span>
 			<input
 				type="checkbox"
-				checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Water')}
+				checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Water')}
 				on:change={(e) => checkHandler(e, 'Water')}
 				value="Water"
-				bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+				bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 			/>
 		</label>
 		<label
 			class={clsx(
 				'absolute bottom-20 text-center',
-				$samplingDesign.samplingLocation.riverCrossSection[0].bed
+				$samplingDesign.samplingLocation.riverCrossSection.bed
 					? 'w-44 left-[15rem]'
 					: 'w-10 left-[19rem]'
 			)}
 		>
 			Bed
 			<select
-				bind:value={$samplingDesign.samplingLocation.riverCrossSection[0].bed}
+				bind:value={$samplingDesign.samplingLocation.riverCrossSection.bed}
 				class="text-sm py-1 px-2 bg-white w-full rounded-md border border-description"
 			>
 				<option value="" disabled />
@@ -108,70 +106,72 @@
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Riffle')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Riffle')}
 					on:change={(e) => checkHandler(e, 'Riffle')}
 					value="Riffle"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Riffle&nbsp; </span>
 			</label>
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Rapids')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Rapids')}
 					on:change={(e) => checkHandler(e, 'Rapids')}
 					value="Rapids"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Rapids&nbsp; </span>
 			</label>
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Levée')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Levée')}
 					on:change={(e) => checkHandler(e, 'Levée')}
 					value="Levée"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Levée&nbsp; </span>
 			</label>
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Oxbow lake')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Oxbow lake')}
 					on:change={(e) => checkHandler(e, 'Oxbow lake')}
 					value="Oxbow lake"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Oxbow lake&nbsp; </span>
 			</label>
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Meander belt')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Meander belt')}
 					on:change={(e) => checkHandler(e, 'Meander belt')}
 					value="Meander belt"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Meander belt&nbsp; </span>
 			</label>
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Flood plain')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Flood plain')}
 					on:change={(e) => checkHandler(e, 'Flood plain')}
 					value="Flood plain"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Flood plain&nbsp; </span>
 			</label>
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Alluvial plain')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes(
+						'Alluvial plain'
+					)}
 					on:change={(e) => checkHandler(e, 'Alluvial plain')}
 					value="Alluvial plain"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Alluvial plain&nbsp; </span>
 			</label>
@@ -180,14 +180,18 @@
 			<label class="flex items-center gap-3">
 				<input
 					type="checkbox"
-					checked={$samplingDesign.samplingLocation.riverCrossSection[0].type.includes('Other area?')}
+					checked={$samplingDesign.samplingLocation.riverCrossSection.type.includes('Other area?')}
 					on:change={(e) => checkHandler(e, 'Other area?')}
 					value="Other area?"
-					bind:group={$samplingDesign.samplingLocation.riverCrossSection[0].type}
+					bind:group={$samplingDesign.samplingLocation.riverCrossSection.type}
 				/>
 				<span class="text-sm shrink-0">Other area?&nbsp; </span>
 			</label>
-			<TextInput label="" placeholder="Please tell us more..." bind:value={$samplingDesign.samplingLocation.riverCrossSection[0].other} />
+			<TextInput
+				label=""
+				placeholder="Please tell us more..."
+				bind:value={$samplingDesign.samplingLocation.riverCrossSection.other}
+			/>
 		</div>
 	</div>
 </Question>
