cdb4e19dc7bb4ed0740078beabf394fb2af3ccf2
Ibrahim Mammadov <zxyctn@gmail.com>
2024-12-04 12:59:58 +0100

    Move last 4 kindom options to the 2nd column and disable options

--- src/lib/components/datasetOverview/Kingdoms.svelte
+++ src/lib/components/datasetOverview/Kingdoms.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
 	import { datasetOverview } from '$lib/stores/datasetOverview';
 	import { KingdomEnum } from '$lib/schemas/datasetOverview';
-
 </script>
 
-<div class="flex flex-col gap-8 text-sm">
-	{#each KingdomEnum.options as value}
-		<label class="flex items-center gap-3">
-			<input type="checkbox" {value} bind:group={$datasetOverview.taxonomicScope.kingdom} name="kingdoms" />
-			<span class="text-sm shrink-0">
-				{value}&nbsp;
-			</span>
-		</label>
-	{/each}
+<div class="grid grid-cols-2 gap-8 text-sm">
+	<div class="flex flex-col gap-8">
+		{#each KingdomEnum.options.slice(0, 3) as value}
+			<label class="flex items-center gap-3">
+				<input
+					type="checkbox"
+					{value}
+					bind:group={$datasetOverview.taxonomicScope.kingdom}
+					name="kingdoms"
+				/>
+				<span class="text-sm shrink-0">
+					{value}&nbsp;
+				</span>
+			</label>
+		{/each}
+	</div>
+	<div class="flex flex-col gap-8">
+		{#each KingdomEnum.options.slice(3) as value}
+			<label class="flex items-center gap-3">
+				<input
+					type="checkbox"
+					{value}
+					bind:group={$datasetOverview.taxonomicScope.kingdom}
+					name="kingdoms"
+					disabled
+				/>
+				<span class="text-sm shrink-0">
+					{value}&nbsp;
+				</span>
+			</label>
+		{/each}
+	</div>
 </div>
